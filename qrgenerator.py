dd574580e5e3ccbe21e60e73e10c237fb876ef83
Ashish Kumar <136437811+Ashish-2911@users.noreply.github.com>
2023-11-21 20:07:55 +0530

    Update qrgenerator.py

--- qrgenerator.py
+++ qrgenerator.py
@@ -10,3 +10,6 @@ fileName = input("What will be your QR file name -  ")
 fileName = fileName + ".png"
 
 obj.png(fileName, scale=6)
+
+# we can improve and add more feature to project.
+# please send me pull request about this project.
