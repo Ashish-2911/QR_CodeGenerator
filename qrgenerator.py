ddc4d31242c609dd2bef2864974cbee7ae2c9bf5
Ashish001 <ashishchouhan57488@gmail.com>
2023-11-21 19:53:00 +0530

    second commit

--- qrgenerator.py
+++ qrgenerator.py
@@ -1,7 +1,12 @@
 import pyqrcode
 
-content = "This Qr code generate by Python."
+# content = "This Qr code generate by Python."
+
+content = input("Enter your value -  ")
 
 obj = pyqrcode.create(content)
 
-obj.png("MyQR.png", scale=6)
+fileName = input("What will be your QR file name -  ")
+fileName = fileName + ".png"
+
+obj.png(fileName, scale=6)
